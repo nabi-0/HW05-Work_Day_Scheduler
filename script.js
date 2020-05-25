73a3878b4b359373a215d7f20eabe49c7ee9d32c
Natalia Arias <abram363@hotmail.com>
2020-05-25 12:10:41 -0500

    script

---
+++ script.js
@@ -0,0 +1,14 @@
+// Formatting Day (fixed)
+var currentDay = moment().format("dddd, MMMM Do");
+$("#currentDay").text(currentDay);
+console.log("Current Day is: " + currentDay);
+
+//Formatting Hour (fixed)
+var currentHour = moment().format("h A");
+console.log("Current Hour is: " + currentHour);
+
+//Array for hours of work (9-5 kind of job)
+var workHours = ["9 AM", "10 AM", "11 AM", "12 PM", "1 PM", "2 PM", "3 PM", "4 PM", "5 PM"];
+console.log("what does this do?")
+
+//
